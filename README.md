5f7bc0824aa9aeed21c50692bf7865b23867829b
niranjana209 <52314670+niranjana209@users.noreply.github.com>
2019-07-03 02:53:51 -0700

    Update and rename README.md to vowel.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# vowel.py
---
+++ vowel.py
@@ -0,0 +1,7 @@
+l = input("input a letter the alphabet :")
+if L in ('a','e','i','o','u'):
+print("% is a vowel"% 1)
+elif l == 'y':
+print("letter y stands for vowel,sometimes stand for consonent.")
+else:
+print("% is a consonent."% 1)
